702300bb04058832107df26b2e41815246c0a146
Joy <jj0985788718@gmail.com>
2024-08-19 16:26:21 +0800

    item columns

--- item/views.py
+++ item/views.py
@@ -3,8 +3,6 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from .models import Item, Category
 from cart.forms import quotationForm
-from django.core.mail import send_mail
-from django.conf import settings
 
 def items(request):
     query = request.GET.get('query', '')
